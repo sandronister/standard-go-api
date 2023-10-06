b0c594e59b7a42de68e87acd9aed3b56c479524e
Julio Sandroni <sandronister@gmail.com>
2023-10-06 18:15:13 -0300

    fix configs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "github.com/sandronister/standart-go-api/configs"
 
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	print(configs.DBDriver)
 }
