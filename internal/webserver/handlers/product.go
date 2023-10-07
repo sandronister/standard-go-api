35f0a1622c6913e31b93eab72b5d160057376ebb
Julio Sandroni <sandronister@gmail.com>
2023-10-07 12:24:00 -0300

    chore improve get product

--- internal/webserver/handlers/product.go
+++ internal/webserver/handlers/product.go
@@ -58,5 +58,7 @@ func (h *productHandler) FindOne(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&product)
 }
